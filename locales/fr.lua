0973368c69e2089a5e2c3c50a8dd3b5b663057c0
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-06 16:27:53 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,1 +1,17 @@
 
+Locales['fr'] = {
+    -- Inventaire
+    ['inventory'] = 'Inventaire',
+    ['weight'] = 'Poids: ',
+    ['slots'] = 'Espaces: ',
+    ['use'] = 'Utiliser',
+    ['drop'] = 'Laisser tomber',
+    ['pickup'] = 'Ramasser',
+    ['close'] = 'Fermer',
+
+    -- Notifications
+    ['item_added'] = 'Vous avez ajouté un(e) ~g~%s~s~ à votre inventaire.',
+    ['item_removed'] = 'Vous avez retiré un(e) ~r~%s~s~ de votre inventaire.',
+    ['no_space'] = 'Vous n\'avez pas assez d\'espace dans votre inventaire.',
+    ['max_weight'] = 'Vous avez atteint le poids maximum dans votre inventaire.',
+}
