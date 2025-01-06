71200c41518fd532dce57bc78bdb4e776bf08d1e
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-06 16:28:40 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+    -- Inventory
+    ['inventory'] = 'Inventory',
+    ['weight'] = 'Weight: ',
+    ['slots'] = 'Slots: ',
+    ['use'] = 'Use',
+    ['drop'] = 'Drop',
+    ['pickup'] = 'Pick up',
+    ['close'] = 'Close',
+
+    -- Notifications
+    ['item_added'] = 'You added a ~g~%s~s~ to your inventory.',
+    ['item_removed'] = 'You removed a ~r~%s~s~ from your inventory.',
+    ['no_space'] = 'You do not have enough space in your inventory.',
+    ['max_weight'] = 'You have reached the maximum weight in your inventory.',
+}
